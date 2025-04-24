67c7cbddf524d759aa911134170e79c16368ab47
Mukesh Murugan <iammukeshm@gmail.com>
2025-04-24 17:53:22 +0530

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,38 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config';
 import starlightThemeNova from 'starlight-theme-nova';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			plugins: [starlightThemeNova()], 
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+    integrations: [
+        starlight({
+            plugins: [starlightThemeNova()],
+            title: 'fullstackhero',
+            customCss: [
+                // Path to your Tailwind base styles:
+                './src/styles/global.css',
+            ],
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+            sidebar: [
+                {
+                    label: 'Guides',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Example Guide', slug: 'guides/example' },
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+            ],
+        }),
 	],
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
